3bb2f055c04d27daaebfb5ed5bd9ac33354740f6
thomasxcarney <thomas.carney26@gmail.com>
2023-08-21 16:45:07 -0400

    Continue working on js for dialog
    Still trying to get the info from the dialog box and implement it
    properly.

--- scripts.js
+++ scripts.js
@@ -1,9 +1,14 @@
 const myLibrary = [];
 const booksContainer = document.querySelector('.books-container');
+
 const addBookButton = document.querySelector('#add-book');
 const addBookDialog = document.querySelector('#add-book-dialog');
 const bookDialog = document.querySelector('#add-book-dialog');
 const confirmBtn = document.querySelector('#confirmBtn');
+
+const titleInput = document.getElementsByName('title');
+const authorInput = document.getElementsByName('author');
+const pagesInput = document.getElementsByName('pages');
 
 function Book(title, author, pages, status) {
     this.title = title
@@ -15,6 +20,7 @@ function Book(title, author, pages, status) {
     };
 };
 
+
 const hobbit = new Book('The Hobbit', 'Tolkien', '300', 'have read');
 const danceDance = new Book('Dance Dance Dance', 'Murakami', '450', 'have read')
 
@@ -24,6 +30,7 @@ function addBookToLibrary(book) {
 };
 
 function displayBooks() {
+    booksContainer.innerHTML = '';
     for(let i = 0; i < myLibrary.length; i++){
         const newDiv = document.createElement('div');
         newDiv.classList.add('book-card', i);
@@ -37,6 +44,15 @@ addBookButton.addEventListener('click', event => {
 });
 
 confirmBtn.addEventListener('click', event => {
+    let status = document.querySelector('input[name="status"]:checked').value;
+    let title = titleInput.value;
+    console.log(titleInput.value);
+    console.log(title);
+    let author = authorInput.value;
+    let pages = pagesInput.value;
+    let newBook = new Book(title, author, pages, status);
+    addBookToLibrary(newBook);
+    displayBooks();
     event.preventDefault();
     bookDialog.close();
 })
